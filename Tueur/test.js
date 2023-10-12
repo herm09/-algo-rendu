aeb52d4eafb257103d9232cff7083f3641dc6102
Hermione Tetard <herm.tetard@gmail.com>
2023-10-13 00:41:35 +0200

    tueur FINI

--- Tueur/test.js
+++ Tueur/test.js
@@ -61,3 +61,60 @@ const Noah = new Survivant('Noah', 'Le nerd', pourcentAleatoire)
 //         liste.splice(indexMort, 1)
 //     }
 // }
+
+//-----------------------------------------------------
+let tueur = {
+    nom: 'Jason',
+    hp: 100
+}
+
+let liste = [];
+let listeMort = [];
+let personnages = ['Le nerd', 'Le sportif', 'Le pompier', 'Le policier', 'Le pêcheur'];
+let prenoms = ['Noah', 'Antoine', 'Yani', 'Léo', 'Dimitri'];
+
+class Survivant {
+    constructor(nom, surnom, pourcentage) {
+        this.nom = prenoms[nom];
+        this.surnom = personnages[surnom];
+        this.pourcentage = pourcentages[pourcentage];
+    }
+}
+
+class Murderer {
+    constructor(nom, health) {
+        this.nom = nom;
+        this.health = health;
+    }
+}
+
+for (let i = 0; i < 5; i++) {
+    let nomAleatoire = Math.floor(Math.random() * 5);
+    let surnomAleatoire = Math.floor(Math.random() * 5);
+    let pourcentAleatoire = Math.floor(Math.random() * 3);
+    liste.push(new Survivant(nomAleatoire, surnomAleatoire, pourcentAleatoire));
+}
+
+while (tueur.hp > 0 && liste.length > 0) {
+    let i = Math.floor(Math.random() * liste.length); // Sélection aléatoire d'un survivant
+
+    // Obtenez le pourcentage de chance de mort pour le survivant actuel
+    let pourcentageMort = liste[i].pourcentage.split(":")[1];
+    pourcentageMort = parseFloat(pourcentageMort);
+
+    if (Math.random() <= pourcentageMort) {
+        // Le survivant est mort
+        listeMort.push(liste[i]);
+        console.log('Jason a tué ' + liste[i].surnom + ' (' + liste[i].nom + ').');
+        liste.splice(i, 1);
+    } else {
+        // Le survivant a survécu
+        console.log(liste[i].surnom + ' (' + liste[i].nom + ') a survécu à Jason.');
+        tueur.hp -= 10; // Par exemple, Jason inflige 10 points de dégâts
+        console.log('Il reste ' + tueur.hp + ' points de vie à Jason');
+    }
+}
+
+console.log('La partie est terminée. Jason ' + (tueur.hp <= 0 ? 'est mort.' : 'a gagné.'));
+console.log('Survivants restants : ', liste);
+console.log('Survivants morts : ', listeMort);
--- Tueur/tueur.js
+++ Tueur/tueur.js
@@ -1,49 +1,82 @@
 let tueur = {
-    name : 'Jason',
-    hp : 100
+    nom: 'Jason',
+    hp: 100
 }
 
 let liste = []
+let listeMort = []
 let personnages = ['Le nerd', 'Le sportif', 'Le pompier', 'Le policier', 'Le pêcheur']
 let prenoms = ['Noah', 'Antoine', 'Yani', 'Léo', 'Dimitri']
 let pourcentages = ["mort: 0.4", "esquive: 0.3", "degats: 0.3"]
 
 class Survivant {
-    constructor(nom, surnom, pourcentage) {
+    constructor(nom, pourcentage) {
         this.nom = prenoms[nom]
         this.surnom = personnages[nom]
         this.pourcentage = pourcentages[pourcentage]
+        this.estMort = false
+    }
+}
+
+class Murderer {
+    constructor(nom, health) {
+        this.nom = nom
+        this.health = health
     }
 }
 
 for (let i = 0; i < 5; i++) {
-    let nomAleatoire = Math.floor(Math.random() * 5)
     let pourcentAleatoire = Math.floor(Math.random() * 3)
-    liste.push(new Survivant(nomAleatoire, nomAleatoire, pourcentAleatoire))
+    liste.push(new Survivant(i, pourcentAleatoire))
 }
 
-console.log(liste)
+while (tueur.hp > 0 && liste.length > 0) {
+    let survivantsNonMorts = liste.filter(survivant => !survivant.estMort)
 
-while (tueur.hp <= 0 && liste.length <= 0) {
-    console.log(liste)
-    let indexMort = -1
+    if (survivantsNonMorts.length === 0) {
+        break //Si tous les survivants sont morts, la boucle s'arrête
+    }
 
-    for (let i = 0; i < liste.length; i ++) {
-        if (liste[i].pourcentage === "mort: 0.4") {
-            indexMort = i
-            liste.splice(indexMort, 1)
-            console.log('Jason a tué ' + liste[indexMort].nom + '.')
-            break
-        } else if (liste[i].pourcentage === "esquive: 0.3") {
-            console.log(liste[i].nom + ' a esquivé Jason. Il lui a infligé 10 de dégats.');
-            tueur.hp -= 10
-            console.log(tueur.hp)
-        } else {
-            indexMort = i
-            liste.splice(i, 1);
-            tueur.hp -= 15
-            console.log(liste[indexMort].nom + ' a infligé 15 de dégats mais est mort.')
-            break
+    let i = Math.floor(Math.random() * survivantsNonMorts.length) //Sélectionne un survivant vivant aléatoirement
+
+    let pourcentageMort = survivantsNonMorts[i].pourcentage.split(":")[1] //divise la string pour ne prendre que le chiffre
+    pourcentageMort = parseFloat(pourcentageMort) //transforme le pourcentage en chiffre
+
+    let randomValue = Math.random()
+
+    if (randomValue <= pourcentageMort) {
+        // Le survivant est mort
+        survivantsNonMorts[i].estMort = true
+        listeMort.push(survivantsNonMorts[i])
+        console.log('Jason a tué ' + survivantsNonMorts[i].nom + ' (' + survivantsNonMorts[i].surnom + ').')
+    } else if (randomValue > pourcentageMort && randomValue <= pourcentageMort + 0.3) {
+        // Le survivant esquive
+        console.log(survivantsNonMorts[i].nom + ' (' + survivantsNonMorts[i].surnom + ') a esquivé Jason. Il lui a infligé 10 de dégâts.')
+        tueur.hp -= 10
+        console.log('Il reste ' + tueur.hp + ' points de vie à Jason')
+    } else {
+        // Le survivant inflige des dégâts à Jason
+        console.log(survivantsNonMorts[i].nom + ' (' + survivantsNonMorts[i].surnom + ') a infligé 15 de dégâts à Jason.')
+        tueur.hp -= 15
+        console.log('Il reste ' + tueur.hp + ' points de vie à Jason')
+    }
+}
+
+if (liste.length === 0) {
+    console.log('Jason a gagné, tous les survivants sont morts!')
+} else if (tueur.hp <= 0) {
+    console.log('Jason est mort, les survivants ont gagné!')
+} else {
+    console.log('Survivants vivants : ', liste.filter(survivant => !survivant.estMort))
+    console.log('Survivants morts : ', listeMort)
+
+    if (listeMort.length > 0) {
+        console.log('Personnages morts :')
+        for (let mort of listeMort) {
+            console.log(mort.nom + ' (' + mort.surnom + ')')
         }
     }
 }
+
+console.log('Survivants vivants : ', liste.filter(survivant => !survivant.estMort))
+console.log('Survivants morts : ', listeMort)
